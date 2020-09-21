bc6c2248d72965608e83cbb418ad5df16281852e
Darrell Wright <beached@users.noreply.github.com>
2020-09-21 01:11:28 -0400

    Added comment parsing

--- config.cpp
+++ config.cpp
@@ -9,5 +9,7 @@
 #include <string_view>
 
 Config parse_config( std::string_view json_document ) {
-	return daw::json::from_json<Config>( json_document );
+	// NO Comments return daw::json::from_json<Config>( json_document );
+	// C++ Sytle Comments 
+	return daw::json::from_json<Config, daw::json::CppCommentSkippingPolicyChecked>( json_document );
 }
